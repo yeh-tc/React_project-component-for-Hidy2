a071931f5f287f1cc5b29ef69c7af2ae360d71aa
yeh <yehtc@ntu.edu.tw>
2023-06-16 11:11:08 +0800

    20230616

--- src/pages/FeedPage.js
+++ src/pages/FeedPage.js
@@ -3,19 +3,13 @@ import Body from '../components/Body';
 import Post from '../components/Posts';
 
 export default function FeedPage() {
-    const [currentTime, setCurrentTime] = useState(0);
-
-    useEffect(() => {
-      fetch('/time')
-      .then(res => res.json())
-      .then(data => setCurrentTime(data.time))
-      
-    }, []);
+    
+    
   
     return(
         <Body sidebar>
             <Post/>
-            <p>The current time is {currentTime}.</p>
+            <p>The current time is .</p>
         </Body>
     );
 }
