b2b78ccd780f30c729a7579d72211bf1374a90c9
Ting-Chen <yehtingchen@gmail.com>
2023-06-17 16:57:27 +0800

    Map.js

--- src/components/map/Map.js
+++ src/components/map/Map.js
@@ -5,21 +5,27 @@ import {
   MapContainer,
   TileLayer,
   GeoJSON,
+  Marker,
   ZoomControl,
   useMapEvent,
 } from "react-leaflet";
 import L from "leaflet";
 import DrawerIcon from "../drawer/DrawerIcon";
 import CircularProgress from "@mui/material/CircularProgress";
-import markerIconPng from "leaflet/dist/images/marker-icon.png";
-import markShadowPng from "leaflet/dist/images/marker-shadow.png";
+import {api} from './api';
+
+
+
 //
+
 
 export default function Map({ onOpenNav }) {
   const [jsonContent, setJsonContent] = useState();
   const [isMapReady, setIsMapReady] = useState(false);
+  const [showLine, setShowLine] = useState(false);
   const mapRef = useRef();
   const geojsonRef = useRef();
+
   //showing lon/lat on map when mouse moving on map
   const [coordinates, setCoordinates] = useState(null);
 
@@ -33,28 +39,34 @@ export default function Map({ onOpenNav }) {
     });
     return null;
   };
-  useEffect(()=>{},[])
+ 
+  useEffect(()=>{
+    fetch(api)
+    .then(data=>data.json())
+    .then(data=>setJsonContent(data))
+    .catch((err) => console.log(`Error Reading data ${err}`));
+  },[])
   useEffect(() => {
     if (mapRef.current && jsonContent) {
       console.log("jsoncontent有改變 就觸發裡面");
-      mapRef.current.fitBounds(geojsonRef.current?.getBounds());
+      //mapRef.current.fitBounds(geojsonRef.current?.getBounds());
     }
   }, [isMapReady, jsonContent]);
 
-  const pointToLayer = useCallback((feature, latlng) => {
-    if (feature.properties && feature.properties.icon) {
-      return L.marker(latlng, {
-        icon: L.icon(feature.properties && feature.properties.icon),
-      });
-    }
-
-    return L.marker(latlng, {
-      icon: L.icon({
-        iconUrl: markerIconPng,
-        shadowUrl: markShadowPng,
-      }),
-    });
-  }, []);
+  //const pointToLayer = useCallback((feature, latlng) => {
+  //  if (feature.properties && feature.properties.icon) {
+  //    return L.marker(latlng, {
+  //      icon: L.icon(feature.properties && feature.properties.icon),
+  //    });
+  //  }
+//
+  //  return L.marker(latlng, {
+  //    icon: L.icon({
+  //      iconUrl: markerIconPng,
+  //      shadowUrl: markShadowPng,
+  //    }),
+  //  });
+  //}, []);
 
   const onEachFeature = useCallback((feature, layer) => {
     if (feature.properties && feature.properties.popupContent) {
@@ -63,6 +75,8 @@ export default function Map({ onOpenNav }) {
       });
     }
   }, []);
+
+
 
   return (
     <Container disableGutters maxWidth="100%" sx={{ mx: 0 }}>
@@ -93,32 +107,36 @@ export default function Map({ onOpenNav }) {
         />
         {/* 顯示航跡 */}
         {jsonContent === undefined ? (
-          <Box
-            position="absolute"
-            top="50%"
-            left="50%"
-            style={{
-              transform: "translate(-50%, -50%)",
-              zIndex: 1000,
-              width: "100%",
-              height: "100%",
-              backgroundColor: "rgba(0,0,0,0.45)",
-              display: "flex",
-              justifyContent: "center",
-              alignItems: "center",
-            }}
-          >
-            <CircularProgress color="secondary" />
-          </Box>
-        ) : (
-          <GeoJSON
-            data={jsonContent}
-            pointToLayer={pointToLayer}
-            onEachFeature={onEachFeature}
-            ref={geojsonRef}
-          />
-        )}
-
+  <Box
+    position="absolute"
+    top="50%"
+    left="50%"
+    style={{
+      transform: "translate(-50%, -50%)",
+      zIndex: 1000,
+      width: "100%",
+      height: "100%",
+      backgroundColor: "rgba(0,0,0,0.45)",
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+    }}
+  >
+    <CircularProgress color="secondary" />
+  </Box>
+) : (
+  <Marker 
+    position={[22.61319, 120.28746]}
+    
+    eventHandlers={{
+      mouseover: () => setShowLine(true),
+      mouseout: () => setShowLine(false),
+    }}
+  >
+    {showLine && <GeoJSON key='line' data={jsonContent} />}
+  </Marker>
+)}
+        
         {/* 放大縮小按鈕 */}
         <ZoomControl position="topright" />
       </MapContainer>
