1d5d5d2c99d85cf0926c56737da1c47246b0e0a6
Ting-Chen <yehtingchen@gmail.com>
2023-06-19 10:14:55 +0800

    Map.js

--- src/components/map/Map.js
+++ src/components/map/Map.js
@@ -1,6 +1,6 @@
 import "../../../node_modules/leaflet/dist/leaflet.css";
 import { useState, useRef, useEffect, useCallback } from "react";
-import { Container, Box } from "@mui/material";
+import { Container, Box, Popover, Typography } from "@mui/material";
 import {
   MapContainer,
   TileLayer,
@@ -12,18 +12,25 @@ import {
 import L from "leaflet";
 import DrawerIcon from "../drawer/DrawerIcon";
 import CircularProgress from "@mui/material/CircularProgress";
-import {api} from './api';
-
-
+import { api } from "./api";
+import Warning from "./Warning";
+import Icon from "./Icon.svg";
 
 //
 
-
 export default function Map({ onOpenNav }) {
+  //api 拿資料成功與否的狀況
+  //也用在等待期間的loader
   const [jsonContent, setJsonContent] = useState();
   const [isMapReady, setIsMapReady] = useState(false);
+  //hover 到第一站的marker會顯示航跡和採樣的站
   const [showLine, setShowLine] = useState(false);
+  const [showPoint, setShowPoint] = useState(false);
+  // popover 是打開與否
+  const [open, setopen] = useState(true);
+  //Ref綁在地圖上
   const mapRef = useRef();
+  //Ref綁在航跡上 使用者點該航跡時 視窗會自動把航跡置中用
   const geojsonRef = useRef();
 
   //showing lon/lat on map when mouse moving on map
@@ -39,13 +46,14 @@ export default function Map({ onOpenNav }) {
     });
     return null;
   };
- 
-  useEffect(()=>{
+  //一開始進來的時候 啟動api fetch
+  useEffect(() => {
     fetch(api)
-    .then(data=>data.json())
-    .then(data=>setJsonContent(data))
-    .catch((err) => console.log(`Error Reading data ${err}`));
-  },[])
+      .then((data) => data.json())
+      .then((data) => setJsonContent(data))
+      .catch((err) => console.log(`Error Reading data ${err}`));
+  }, []);
+  //map準備好&有jsonContent 即啟動
   useEffect(() => {
     if (mapRef.current && jsonContent) {
       console.log("jsoncontent有改變 就觸發裡面");
@@ -59,7 +67,7 @@ export default function Map({ onOpenNav }) {
   //      icon: L.icon(feature.properties && feature.properties.icon),
   //    });
   //  }
-//
+  //
   //  return L.marker(latlng, {
   //    icon: L.icon({
   //      iconUrl: markerIconPng,
@@ -75,8 +83,11 @@ export default function Map({ onOpenNav }) {
       });
     }
   }, []);
-
-
+  const id = open ? "simple-popover" : undefined;
+  const handleClose = () => {
+    setJsonContent();
+  };
+  console.log(jsonContent);
 
   return (
     <Container disableGutters maxWidth="100%" sx={{ mx: 0 }}>
@@ -106,37 +117,34 @@ export default function Map({ onOpenNav }) {
           attribution="Tiles &copy; Esri"
         />
         {/* 顯示航跡 */}
+        {/* 資料還沒進來時 顯示loader */}
         {jsonContent === undefined ? (
-  <Box
-    position="absolute"
-    top="50%"
-    left="50%"
-    style={{
-      transform: "translate(-50%, -50%)",
-      zIndex: 1000,
-      width: "100%",
-      height: "100%",
-      backgroundColor: "rgba(0,0,0,0.45)",
-      display: "flex",
-      justifyContent: "center",
-      alignItems: "center",
-    }}
-  >
-    <CircularProgress color="secondary" />
-  </Box>
-) : (
-  <Marker 
-    position={[22.61319, 120.28746]}
-    
-    eventHandlers={{
-      mouseover: () => setShowLine(true),
-      mouseout: () => setShowLine(false),
-    }}
-  >
-    {showLine && <GeoJSON key='line' data={jsonContent} />}
-  </Marker>
-)}
+          <Box
+            position="absolute"
+            top="50%"
+            left="50%"
+            style={{
+              transform: "translate(-50%, -50%)",
+              zIndex: 1000,
+              width: "100%",
+              height: "100%",
+              backgroundColor: "rgba(0,0,0,0.45)",
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+            }}
+          >
+            <CircularProgress color="secondary" />
+          </Box>
+        ) : /* api返回沒有資料 會有Popover 提醒沒資料 */
         
+        jsonContent === "No result" ? (
+          <Warning />
+        ) : (
+          /* api返回資料 */
+          <CircularProgress color="secondary" />
+        )}
+
         {/* 放大縮小按鈕 */}
         <ZoomControl position="topright" />
       </MapContainer>
