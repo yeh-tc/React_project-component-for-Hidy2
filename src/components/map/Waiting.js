821d38ba412891f47919086aec3e3818bc74c897
Ting-Chen <yehtingchen@gmail.com>
2023-06-20 10:48:27 +0800

    Waiting.js

---
+++ src/components/map/Waiting.js
@@ -0,0 +1,24 @@
+import {Box } from "@mui/material";
+import CircularProgress from "@mui/material/CircularProgress";
+export default function Waiting() {
+  return(
+    
+  <Box
+    position="absolute"
+    top="50%"
+    left="50%"
+    sx={{
+      transform: "translate(-50%, -50%)",
+      zIndex: 1000,
+      width: "100%",
+      height: "100%",
+      backgroundColor: "rgba(0,0,0,0.45)",
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+    }}
+  >
+    <CircularProgress color="secondary" />
+  </Box>
+  );
+}
