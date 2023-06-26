63d0cc7fed42db55866421c68aa0735d8b169c7c
Ting-Chen <yehtingchen@gmail.com>
2023-06-26 13:52:15 +0800

    Drawer.js

--- src/components/drawer/Drawers.js
+++ src/components/drawer/Drawers.js
@@ -1,15 +1,29 @@
-import { Box, Drawer, Typography } from "@mui/material";
+import {
+  Box,
+  Drawer,
+  Divider,
+  List,
+  Typography,
+} from "@mui/material";
+import React from "react";
 import useResponsive from "../../hooks/useResponsive";
 import Scrollbar from "../scrollbar/Scrollbar";
 import Logo from "../logoname/Logo";
 import SelectCruise from "../filter/SelectCruise";
 
+import { useState } from "react";
+import CruiseInfo from "./CruiseInfo";
 const nav_width = 280;
 
-export default function Drawers({ openNav, onCloseNav }) {
+export default function Drawers({ openNav, onCloseNav, selectCruiseId, setActiveCruise  }) {
   const isDesktop = useResponsive("up", "md");
-  
-  
+  const [open, setOpen] = useState(false);
+  const handleClick = (index) => {
+    setOpen({
+      ...open,
+      [index]: !open[index],
+    });
+  };
   const renderContent = (
     <Scrollbar
       sx={{
@@ -26,12 +40,23 @@ export default function Drawers({ openNav, onCloseNav }) {
       </Box>
       <Typography
         variant="body2"
-        sx={{ pl: 2, fontFamily: "Public Sans, sans-serif"}}
+        sx={{ pl: 2, fontFamily: "Public Sans, sans-serif" }}
       >
         Cruise Filter
       </Typography>
       <SelectCruise />
-
+      <Divider />
+      <List
+        sx={{ width: "100%", maxWidth: 360, bgcolor: "background.paper" }}
+        component="nav"
+      >
+        {selectCruiseId !== null &&
+          selectCruiseId.map((text, index) => (
+            <React.Fragment key={index}>
+              <CruiseInfo index={index} text={text} open={open} handleClick ={()=>handleClick(index)} setActiveCruise={setActiveCruise} />
+            </React.Fragment>
+          ))}
+      </List>
     </Scrollbar>
   );
 
