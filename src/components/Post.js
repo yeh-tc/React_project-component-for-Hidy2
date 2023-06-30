8327aa5df82dd53321f24eb0c2bd590958a2d32a
Ting-Chen <yehtingchen@gmail.com>
2023-06-30 10:01:55 +0800

    .

--- src/components/Post.js
+++
@@ -1,5 +0,0 @@
-export default function Post({post}){
-    return(
-        <p>test</p>
-    );
-}
--- src/pages/LoginPage.js
+++
@@ -1,9 +0,0 @@
-import Body from '../components/Body';
-export default function LoginPage(){
-    return(
-    <Body>
-    <h1>Login</h1>
-    <p>username?</p>
-    </Body>
-    );
-}
