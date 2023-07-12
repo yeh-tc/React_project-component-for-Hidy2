46d45a81175fe103bf47a08d595bc0e975a2a1a9
Ting-Chen <yehtingchen@gmail.com>
2023-07-12 16:44:42 +0800

    RemindParaWarning.js

---
+++ src/components/chemistry/RemindParaWarning.js
@@ -0,0 +1,33 @@
+import * as React from "react";
+import { useState } from "react";
+import Snackbar from "@mui/material/Snackbar";
+import Alert from "@mui/material/Alert";
+
+
+export default function RemindParaWarning() {
+  const [open, setOpen] = useState(true);
+  
+
+  const handleClose = (event, reason) => {
+    if (reason === "clickaway") {
+      return;
+    }
+    setOpen(false);
+  };
+
+  return (
+    <div>
+      <Snackbar
+        anchorOrigin={{ vertical: "top", horizontal: "center" }}
+        open={open}
+        autoHideDuration={6000}
+        onClose={handleClose}
+        sx={{ zIndex: 999 }}
+      >
+        <Alert onClose={handleClose} severity="warning">
+        Please choose the parameters on the left panel.
+        </Alert>
+      </Snackbar>
+    </div>
+  );
+}
