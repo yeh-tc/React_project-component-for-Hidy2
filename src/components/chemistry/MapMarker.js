910e06d1600f89ffedeb938b43636555b1f855b2
Ting-Chen <yehtingchen@gmail.com>
2023-07-19 12:23:31 +0800

    MapMarker.js

--- src/components/chemistry/MapMarker.js
+++ src/components/chemistry/MapMarker.js
@@ -1,38 +1,42 @@
-import { useState } from "react";
+import { useState, useMemo } from "react";
 import { Marker, Tooltip } from "react-leaflet";
-import { Modal, Typography, Box, Stack, IconButton } from "@mui/material";
 import L from "leaflet";
 import stationsvg from "./SVG/blue_small_1.svg";
 import orangesvg from "./SVG/orange_small_1.svg";
 import greysvg from "./SVG/grey_small.svg";
-import ArrowCircleLeftIcon from "@mui/icons-material/ArrowCircleLeft";
-import ArrowCircleRightIcon from "@mui/icons-material/ArrowCircleRight";
-const style = {
-  position: "absolute",
-  top: "50%",
-  left: "50%",
-  transform: "translate(-50%, -50%)",
-  width: "60%",
-  height: "60%",
-  bgcolor: "background.paper",
-  border: "2px solid #000",
+import StationModal from "./StationModal";
 
-  boxShadow: 24,
-  p: 4,
-};
+
+
+const MarkerIcon = ({ isClicked, isHovered })=> useMemo(()=>{
+  const iconUrl = isClicked ? greysvg : isHovered ? orangesvg : stationsvg;
+  return L.icon({
+  iconUrl,
+  iconSize: [12, 12],
+  iconAnchor: [5.6, 7],
+  });
+},[isClicked,isHovered])
+
+const TooltipContent = ({object,index}) => useMemo(()=>(
+  <>
+  {object.properties.date} &nbsp; Station:{object.properties.station}<br/>
+  Click for data display.index:{index}
+  </>
+),[object])
+
+
 export default function MapMarker({
   object,
   isClicked,
   isHovered,
-  id,
   setActiveHover,
+  index,
 }) {
   const [open, setOpen] = useState(false);
-  const handleClose = () => {
-    console.log(object);
-    setOpen(false);
-  };
 
+  const markerIcon = MarkerIcon({ isClicked, isHovered });
+  const tooltipContent = TooltipContent({object,index});
+  
   return (
     <>
       <Marker
@@ -40,76 +44,19 @@ export default function MapMarker({
           object.geometry.coordinates[0],
           object.geometry.coordinates[1],
         ]}
-        icon={L.icon({
-          iconUrl: isClicked ? greysvg : isHovered ? orangesvg : stationsvg,
-          iconSize: [12, 12],
-          iconAnchor: [5.6, 7],
-        })}
+        icon={markerIcon}
         eventHandlers={{
-          mouseover: () => setActiveHover(id),
+          mouseover: () => setActiveHover(object.properties.odb_cruise_id),
           mouseout: () => setActiveHover(null),
           click: () => setOpen(true),
         }}
         zIndexOffset={isClicked ? 9999 : isHovered ? 9999 : 500}
       >
         <Tooltip direction="top" offset={[0, 0]}>
-          {object.properties.date} &nbsp; Sta:{object.properties.station}<br/>
-          Click for data display.
+        {tooltipContent}
         </Tooltip>
       </Marker>
-      <Modal
-        open={open}
-        onClose={handleClose}
-        aria-labelledby="modal-modal-title"
-        aria-describedby="modal-modal-description"
-      >
-        <Box sx={style}>
-          <Stack
-            direction="row"
-            justifyContent="flex-start"
-            alignItems="center"
-            spacing={3}
-          >
-            <Typography
-              id="modal-modal-title"
-              variant="h6"
-              component="h2"
-              sx={{ color: "#1976d2", fontWeight: 600 }}
-            >
-              {object.properties.odb_cruise_id}
-            </Typography>
-            <Stack
-              direction="row"
-              justifyContent="flex-start"
-              alignItems="center"
-            >
-              <IconButton sx={{ color: "#1976d2" }}>
-                <ArrowCircleLeftIcon />
-              </IconButton>
-              <Typography variant="h6" sx={{ color: "#1976d2" }}>
-                {object.properties.station}
-              </Typography>
-              <IconButton sx={{ color: "#1976d2" }}>
-                <ArrowCircleRightIcon />
-              </IconButton>
-            </Stack>
-          </Stack>
-          <Stack>
-            <Typography sx={{ mt: 1,color:'#565360' }}>
-              Date&nbsp;(GMT+8):&nbsp;{object.properties.date}&nbsp;{object.properties.time}
-            </Typography>
-            <Typography sx={{ mt: 1 ,color:'#565360'}}>
-              Location:&nbsp;{object.geometry.coordinates[1]},{object.geometry.coordinates[0]}
-            </Typography>
-            <Typography  sx={{ mt: 1 ,color:'#565360'}}>
-              Maximum Depth:&nbsp;{object.properties.depth}&nbsp;m
-            </Typography>
-          </Stack>
-          <Typography id="modal-modal-description" sx={{ mt: 2 }}>
-            Data
-          </Typography>
-        </Box>
-      </Modal>
+      <StationModal open={open} setOpen={setOpen} object={object}/>
     </>
   );
 }
